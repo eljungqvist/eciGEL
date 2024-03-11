9b1a6181971bd5d57d4de7078c1794c8d04a878e
eljungqvist <33607261+eljungqvist@users.noreply.github.com>
2024-03-11 13:37:22 +0100

    Update calculate_kapp.m

--- code/Proteomics/calculate_kapp.m
+++ code/Proteomics/calculate_kapp.m
@@ -8,17 +8,20 @@ C13fluxes = readtable('../../../Databases/Cordova_fluxes_rxns.xlsx', 'ReadVariab
 C13fluxes.Flux = C13fluxes.Flux.*(30.6/100);
 
 % Find CCM reactions in ecModel
-CCM.Reactions = ecModel.ec.rxns(find(contains(ecModel.ec.rxns, C13fluxes.RxnID)));
-CCM.Reactions(contains(CCM.Reactions, 'REV')) = '';
-CCM.IDXs = find(ismember(ecModel.ec.rxns, CCM.Reactions));
+CCM.Reactions = C13fluxes.RxnID; %
+CCM.Flux = C13fluxes.Flux;
+
+for i=1:length(CCM.Reactions)
+    CCM.IDXs(i) = find(ismember(ecModel.ec.rxns, CCM.Reactions(i)));
+end
+CCM.IDXs = CCM.IDXs';
+
+%%
 idx2remove=[];
 % Find protein abundances
 for i=1:length(CCM.Reactions)
     if sum(ecModel.ec.rxnEnzMat(CCM.IDXs(i), :))>1 % multi-subunit enzymes, remove.
         idx2remove = [idx2remove; i];
-        % Subunits = ecModel.ec.enzymes(find(ecModel.ec.rxnEnzMat(CCM.IDXs(i), :)));
-        % CCM.abundance(i) = sum(Proteomics.abundances(contains(Proteomics.uniprotIDs, Subunits)));
-        % CCM.mw(i) = sum(ecModel.ec.mw(contains(ecModel.ec.enzymes, Subunits)));
     else
         Protein = ecModel.ec.enzymes(find(ecModel.ec.rxnEnzMat(CCM.IDXs(i), :)));
         CCM.abundance(i) = Proteomics.abundances(contains(Proteomics.uniprotIDs, Protein));
@@ -29,16 +32,10 @@ CCM.Reactions(idx2remove) = '';
 CCM.IDXs(idx2remove) = '';
 CCM.abundance(idx2remove) = '';
 CCM.mw(idx2remove) = '';
-
+CCM.Flux(idx2remove) = '';
+%%
 CCM.abundance = CCM.abundance';
 CCM.mw = CCM.mw';
-
-% Add fluxes
-for i=1:length(C13fluxes.RxnID)
-    Structidxs = find(contains(CCM.Reactions, C13fluxes.RxnID(i)));
-    CCM.flux(Structidxs) = C13fluxes.Flux(i);
-end
-CCM.flux = CCM.flux';
 
 % Convert enzyme abundance from mg/gDW to mmol/gDW
 CCM.abundance = CCM.abundance./CCM.mw;
@@ -53,12 +50,10 @@ for i=1:length(IsoRxns)
 end
 
 % Calculate kapp
-CCM.kapp = CCM.flux./CCM.abundance;
+CCM.kapp = CCM.Flux./CCM.abundance;
 
 % Rescale kapp to s-1 from h-1
 CCM.kapp = CCM.kapp/3600;
 
 kapptable = table(CCM.Reactions, CCM.kapp, 'VariableNames', {'Reactions', 'kapp'});
 writetable(kapptable, '../../../Databases/kapp.tsv', 'filetype','text', 'delimiter','\t');
-
-
